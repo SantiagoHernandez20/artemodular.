906db57df70abb1dcf4f1f4fd42576e314941350
Santiago <jairsantiagomh@gmail.com>
2025-09-08 17:57:32 -0500

    refactor: update vite.confif.js

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,31 @@ import { defineConfig, loadEnv } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
-// Migración desde vue.config.js
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  
   return {
     plugins: [vue()],
-
+    
+    // Especificar la ubicación del index.html
+    root: '.',
+    publicDir: 'public',
+    
     // Configuración del servidor de desarrollo
     server: {
       port: parseInt(env.VITE_APP_FRONTEND_PORT || '') || 9001,
     },
-
+    
     // Configuración de compilación
     build: {
+      outDir: 'dist',
       sourcemap: env.NODE_ENV !== 'production',
     },
-
+    
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    },
-
-    // En Vite, las variables se leen desde import.meta.env
-    // Se elimina el mapeo heredado del entorno clásico
+    }
   }
 })
